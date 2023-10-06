dca353775e90056c637f620d0ca9092ca8d1208b
Veeknd <sethumanikkams@gmail.com>
2023-10-06 14:00:03 +0530

    :pencil: code improvement

--- db.py
+++ db.py
@@ -1,14 +1,15 @@
 import mysql.connector
+from decouple import config
 
 database = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
-    passwd = 'Doforit16'
+    passwd = config('passwd')
 
 )
 
 curserObject = database.cursor()
 
-curserObject.execute("CREATE DATABASE Notes")
+curserObject.execute("CREATE DATABASE IF NOT EXISTS Notes")
 
 print("all done")
