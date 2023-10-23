ea2a7505dde18ef3eaefc7b033d6167096c8d919
Veeknd <sethumanikkams@gmail.com>
2023-10-23 12:15:53 +0530

    fix bug.. anonymous user error

--- noteList/owner.py
+++ noteList/owner.py
@@ -1,6 +1,6 @@
 from django.views.generic import CreateView, UpdateView, ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.contrib.auth.models import User
 class OwnerCreateView (LoginRequiredMixin, CreateView):
 
     def form_valid(self,form):
@@ -15,5 +15,14 @@ class OwnerListView(ListView):
     def get_queryset(self):
         print('update get_queryset called')
         """ Limit a User to only modifying their own data. """
-        qs = super(OwnerListView, self).get_queryset()
-        return qs.filter(owner=self.request.user)
+        
+        # if not self.request.user.is_anonymous:
+        #     temp = User.objects.create_user(username="anon", email="none", first_name="none", last_name="none")
+        #     temp.save()
+        #     anonUser = User.objects.get(username='anon')
+        #     self.request.user=User.objects.get(id=anonUser.id)
+        qs = None
+        if self.request.user.is_authenticated:
+            qs = super(OwnerListView, self).get_queryset()
+            qs = qs.filter(owner=self.request.user)
+        return qs
